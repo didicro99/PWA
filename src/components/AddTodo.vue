df2e55412961566e359cba4dc2741eca566e41b9
didicro99 <64322841+didicro99@users.noreply.github.com>
2020-12-07 01:17:34 +0100

    Zadatak 2

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="app" :class="typeof weather.main !='undefined'&& this.pic.endsWith('n') ? 'noc':''">
         <header>
                 <nav>
                     <div class="logo">
@@ -15,17 +15,20 @@
             </header>
         <main>
             <div class="okvir">
-                <input type="text" class="traka" placeholder="Search City..."/>
+                <input type="text" class="traka" placeholder="Upisite ime grada..." v-model="query" @keypress="fetchWeather"/>
             </div>
-            <div class="Podaci">
+            <div class="Podaci" v-if="typeof weather.main !='undefined'">
                 <div class="LokacijaVrijeme">
-                    <div class="Lokacija">Zagreb,CRO</div>
-                    <div class="Vrijeme">Subota, 31.10.2020</div>
+                    <div class="Lokacija">{{weather.name}},  {{weather.sys.country}}</div>
+                    <div class="Vrijeme">{{dateBuilder()}}</div>
+                    <div class="Vrijeme">{{lastupdatevalue}}</div>
                 </div>
                 <div class="TempStanje">
-                    <div class="Temperatura">15°C</div>
-                    <div><img src="../assets/svg/wi-day-sunny.svg" alt="Sunce" width="200" height="200"></div>
-                    <div class="Stanje">Sunčano</div>
+                    <div class="Temperatura">{{Math.round(weather.main.temp)}}°C<br/>
+                        <img :src="'https://openweathermap.org/img/wn/'+this.pic+'@4x.png'"/><br/>
+                        <div class="Stanje">{{weather.weather[0].description}}</div>
+                    </div>
+                    <div class="Vjetarivlaznost">Vjetar: {{Math.round(weather.wind.speed)}}  km/h <span class="tab"></span> Vlažnost zraka: {{Math.round(weather.main.humidity)}}%</div>
                 </div>
             </div>
         </main>
@@ -37,6 +40,45 @@
 
 <script>
 export default {
+    data(){
+        return{
+            api_key:'309ce81e0c0cfabb8d51b5fac2506df5',
+            url_base:'http://api.openweathermap.org/data/2.5/',
+            query:'',
+            weather:{},
+            pic: {},
+        }
+    },
+    methods:{
+        fetchWeather (e){
+            if(e.key=="Enter"){
+                fetch(`${this.url_base}weather?q=${this.query}&units=metric&lang=hr&APPID=${this.api_key}`)
+                    .then(res=> {
+                        return res.json();
+                    }).then(this.setResults);
+            }
+        },
+        setResults(results){
+            this.weather=results;
+            this.pic =results.weather[0].icon
+        },
+        dateBuilder (){
+            let d = new Date()
+            let months=["Siječanj","Veljača","Ožujak","Travanj",
+            "Svibanj","Lipanj","Srpanj","Kolovoz",
+            "Rujan","Listopad","Studeni","Prosinac"];
+
+            let days=["Ponedjeljak","Utorak","Srijeda",
+            "Četvrtak","Petak","Subota","Nedjelja"];
+
+            let day= days[d.getDay()];
+            let date= d.getDate();
+            let month= months[d.getMonth()];
+            let year= d.getFullYear();
+
+            return `${day}, ${date}. ${month} ${year}`;
+        }
+    }
 }
 </script>
 <style>
@@ -79,25 +121,31 @@ body {
     font-family:"montserrat", sans-serif;
 }
 #app {
-    background-image: url('../assets/pozadina.jpg');
     background-size:cover;
     background-position: top;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.3)),url('../assets/pozadina.jpg');
 }
+#app .noc{
+    background-size:cover;
+    background-position: top;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.9)),url('../assets/pozadina.jpg');
+}
+
 main {
     font-family:"montserrat", sans-serif;
     min-height: 100vh;
     padding:25px;
-    background-image: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.5));
 }
 .okvir{
     width:100;
     margin-bottom: 30px;
+
 }
 .okvir .traka{
     display:block;
     width:100%;
     padding:15px;
-    color:peru;
+    color:black;
     font-size:20px;
 
     appearance: none;
@@ -144,17 +192,28 @@ main {
     margin: 30px 0px;
     box-shadow: 5px 8px rgba(0,0,0,0.5);
 }
-img{
-    filter: invert(100%);
-    margin:-20px;
-}
+
 .TempStanje .Stanje{
     color:white;
     font-size:40px;
     font-weight:700;
     font-style:italic;
     text-shadow: 4px 5px rgba(0,0,0,0.25);
+    margin:-70px 0px 0px 0px;
 }
+
+.TempStanje .Vjetarivlaznost{
+    color:white;
+    font-size:27px;
+    font-weight:600;
+    text-shadow: 2px 4px rgba(0,0,0,0.25);
+    margin-top:20px;
+}
+ .tab { 
+            display: inline-block; 
+            margin-left: 25px; 
+        }
+
 footer{
     background-color: black;
     display:flex;
